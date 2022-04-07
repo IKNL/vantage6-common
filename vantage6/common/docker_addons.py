e324005acae69b83868a69dae2faa30f1dcbb1fe
bart <b.vanbeusekom@iknl.nl>
2022-04-07 11:59:38 +0200

    Rabbitmq code changes from v3 to v2

--- vantage6/common/docker_addons.py
+++ vantage6/common/docker_addons.py
@@ -5,12 +5,14 @@ import requests
 import base64
 import json
 import signal
+import pathlib
 
 from dateutil.parser import parse
 from requests.api import request
 
 from vantage6.common import logger_name
 from vantage6.common import ClickLogger
+from vantage6.common.globals import APPNAME
 
 logger = logger_name(__name__)
 log = logging.getLogger(logger)
@@ -231,3 +233,29 @@ def pull_if_newer(docker_client, image: str, log=ClickLogger):
         except docker.errors.APIError as e:
             log.error(f"Failed to pull image! {image}")
             log.debug(e)
+
+
+def running_in_docker() -> bool:
+    """Return True if this code is executed within a Docker container."""
+    return pathlib.Path('/.dockerenv').exists()
+
+
+def get_server_config_name(container_name: str, scope: str):
+    """
+    Get the configuration name of a server from its docker container name
+    Docker container name of the server is formatted as
+    f"{APPNAME}-{self.name}-{self.scope}-server". This will return {self.name}
+    Parameters
+    ----------
+    container_name: str
+        Name of the docker container in which the server is running
+    scope: str
+        Scope of the server (e.g. 'system' or 'user')
+    Returns
+    -------
+    str
+        A server's configuration name
+    """
+    idx_scope = container_name.rfind(scope)
+    length_app_name = len(APPNAME)
+    return container_name[length_app_name+1:idx_scope-1]
